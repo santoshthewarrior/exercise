144d71671bc9185bc9bf4ee84615a4b221fd711e
santoshthewarrior <santoshthewarrior@hotmail.com>
2015-08-12 15:18:47 +0545

    Third commit

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -9,11 +9,11 @@ use App\Http\Controllers\Controller;
 class EmailController extends Controller{
 	public function index(Request $request){
 		 $user = 'Santosh';
-		\Config::set('mail.driver', 'smtp');
-		\Config::set('mail.host', 'smtp.gmail.com');
-		\Config::set('mail.username', 'santoshthewarrior45@gmail.com'); // gmail username
-		\Config::set('mail.password', 'schntheo'); // gmail password
-		\Config::set('mail.encryption', 'tls');
+		 Config::set('mail.driver', 'smtp');
+		 Config::set('mail.host', 'smtp.gmail.com');
+		 Config::set('mail.username', 'santoshthewarrior45@gmail.com'); // gmail username
+		 Config::set('mail.password', 'schntheo'); // gmail password
+		 Config::set('mail.encryption', 'tls');
 
         //Mail::send('emails.test',['testVar' => 'Just a silly test'],function($message) { 
         	//$message->to('santoshthewarrior@hotmail.com')->subject('A simple test');
